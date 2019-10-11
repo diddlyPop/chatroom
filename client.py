4f3d6edeb93ff538ed8a90ef1c8012f73e3acae5
Kyle Guss <kyleguss@icloud.com>
2019-10-11 11:09:21 -0700

    gui's working, need client/server setup now

--- client.py
+++ client.py
@@ -1,4 +1,9 @@
 import PySimpleGUI as simpleg
+import socket
+import os
+
+def send_message(message):
+    pass
 
 def launch_login():
     layout = [
@@ -7,19 +12,26 @@ def launch_login():
         [simpleg.Submit(), simpleg.Cancel()]
     ]
     window = simpleg.Window('Login').Layout(layout)
-    button, values = window.Read()
+    event, values = window.Read()
     user = values[0]
     window.close()
     return user
 
 def launch_chatbox(user):
     layout = [
-        [simpleg.Text('%s: ' % user, size=(15, 1)), simpleg.InputText('')],
+        [simpleg.Output(size=(80, 20), background_color="black", text_color="white")],
+        [simpleg.Text('%s: ' % user, size=(15, 1)), simpleg.InputText('', do_not_clear=False)],
         [simpleg.Submit('Send'), simpleg.Cancel()]
     ]
     window = simpleg.Window('Chat').Layout(layout)
-    button, values = window.Read()
-
+    while True:
+        event, values = window.Read()
+        if event in (None, 'Cancel'):
+            break
+        else:
+            print("{0}: {1}".format(user, values[0]))
+            send_message(values[0])
+    window.Close()
 
 if __name__ == '__main__':
     user = launch_login()
