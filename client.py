16fd3550ca89865b86df9b483d04761691896024
= <kyleguss@icloud.com>
2019-11-11 15:27:57 -0800

    started working on dynamic color changes, updated version

--- client.py
+++ client.py
@@ -8,7 +8,9 @@ chatroom client
 import PySimpleGUI as sg
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-#from sniffrr import Sniffrr
+from datetime import datetime
+import random
+# from sniffrr import Sniffrr
 
 
 def launch_login(retry):     # displays login prompt and returns user login
@@ -28,15 +30,8 @@ def launch_login(retry):     # displays login prompt and returns user login
 
 
 def launch_chatbox(user):   # displays chatbox with custom user from login, calls send_message
-    layout = [
-        [sg.Output(size=(80, 20), background_color="black", text_color="white")],
-        [sg.Text('%s: ' % user, size=(15, 1)), sg.InputText('', do_not_clear=False)],
-        [sg.Submit('Send'), sg.Quit(), sg.Button("dog", button_color=sg.TRANSPARENT_BUTTON,
-               image_filename="dog.png", image_size=(50, 50), image_subsample=2, border_width=0)]
-    ]
-    window = sg.Window('Chat').Layout(layout)
     while True:
-        event, values = window.Read()
+        event, values = window_main.Read()
         if event in (None, 'Quit'):
             send_message("/quit")  # sends out QUIT message from our client
             break
@@ -44,7 +39,7 @@ def launch_chatbox(user):   # displays chatbox with custom user from login, call
             send_message("\n ^..^          /\n/_/\_____/\n     /\   /\\\n    /  \ /  \\")
         else:
             send_message(values[0])   # SUBMIT sends out message from our client
-    window.close()
+    window_main.close()
 
 
 def send_message(message_to_send):    # send_message transmits client username and message
@@ -53,11 +48,26 @@ def send_message(message_to_send):    # send_message transmits client username a
 
 def receive_message():
     while True:
+        now = datetime.now()
+        time = "[" + now.strftime("%H:%M:%S") + "] "
         try:
             received_msg = connection.recv(BUFFERSIZE).decode("utf8")
-            print(received_msg)
+            printcolor(received_msg[0], time + received_msg)
         except OSError:  # client left
             break
+
+
+def printcolor(sender, message):
+    back_color = "black"
+    try:
+        sender.lower()
+        # TODO generate random color from 'colors' list
+    except:
+        back_color = "black"
+    txt = window_main.FindElement("protocol").Widget
+    txt.tag_config(sender, background=back_color, foreground="white")
+    txt.insert("end", message + "\n", sender)
+    window_main.FindElement("protocol").Update("", append=True)
 
 
 # def sniffing():
@@ -74,8 +84,10 @@ TCP_IP = "167.71.156.224"
 LOCAL_IP = "127.0.0.1"
 TCP_PORT = 33001
 TCP_ADDRESS = (TCP_IP, TCP_PORT)
-AUTH_TOKEN = "1109"
+AUTH_TOKEN = "1111"
 BUFFERSIZE = 1024
+
+colors = ["black", "red", "yellow", "darkblue"]
 
 user = launch_login(False)   # get user from login window
 
@@ -100,6 +112,16 @@ receive_thread.start()
 
 # sniff_thread = Thread(target=sniffing)          # requires Scapy
 # sniff_thread.start()
+
+# TODO need to figure out how to stop user from being able to alter multiline box object on PySimpleGui docs
+layout_main = [
+    [sg.Multiline("", key="protocol", autoscroll=True, size=(80, 20))],
+    [sg.Text('%s: ' % user, size=(15, 1)), sg.InputText('', do_not_clear=False)],
+    [sg.Submit('Send'), sg.Quit(), sg.Button("dog", button_color=sg.TRANSPARENT_BUTTON,
+                                             image_filename="dog.png", image_size=(50, 50), image_subsample=2,
+                                             border_width=0)]
+]
+window_main = sg.Window('Chat').Layout(layout_main)
 
 launch_chatbox(user)    # start chatbox gui under client username
 
