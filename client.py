17a2a660d9e2cce40277b05c78399a1ef0a7fc1b
Kyle Guss <kyleguss@icloud.com>
2019-11-15 10:25:17 -0800

    cleaned up code styling

--- client.py
+++ client.py
@@ -12,66 +12,106 @@ from datetime import datetime
 import random
 # from sniffrr import Sniffrr
 
-
-def launch_login(retry):     # displays login prompt and returns user login
-    login_text = "Please choose a username to chat under."
-    if retry:
-        login_text = "Username is taken or client is out-of-date."
-    layout = [
-        [sg.Text(login_text)],
-        [sg.Text('Username: ', size=(15, 1)), sg.InputText('')],
-        [sg.Submit(), sg.Quit()]
-    ]
-    window = sg.Window('Login').Layout(layout)
-    event, values = window.Read()
-    if values[0] is None:
-        exit()
-    user = values[0]
-    window.close()
-    return user
+TCP_IP = "167.71.156.224"
+LOCAL_IP = "127.0.0.1"
+TCP_PORT = 33001
+TCP_ADDRESS = (LOCAL_IP, TCP_PORT)
+AUTH_TOKEN = "1111"
+BUFFERSIZE = 1024
 
 
-def launch_chatbox(user):   # displays chatbox with custom user from login, calls send_message
-    while True:
-        event, values = window_main.Read()
-        if event in (None, 'Quit'):
-            send_message("/quit")  # sends out QUIT message from our client
-            break
-        elif event == "dog":
-            send_message("\n ^..^          /\n/_/\_____/\n     /\   /\\\n    /  \ /  \\")
-        else:
-            send_message(values[0])   # SUBMIT sends out message from our client
-    window_main.close()
+class Client:
 
+    def __init__(self):
+        self.user = None
+        self.buttons = {"dog": "\n ^..^          /\n/_/\_____/\n     /\   /\\\n    /  \ /  \\"}
+        self.colors = ["black", "red", "green", "darkblue"]
+        self.connection = None
+        self.__setup_flow()
+        self.layout_main = [
+            [sg.Multiline("", key="protocol", autoscroll=True, size=(80, 20))],
+            [sg.Text('%s: ' % self.user, size=(15, 1)), sg.InputText('', do_not_clear=False)],
+            [sg.Submit('Send'), sg.Quit(), sg.Button("dog", button_color=sg.TRANSPARENT_BUTTON,
+                                                     image_filename="dog.png", image_size=(50, 50), image_subsample=2,
+                                                     border_width=0)]
+        ]
+        self.window_main = sg.Window('Chat').Layout(self.layout_main)
+        self.launch_chatbox()
 
-def send_message(message_to_send):    # send_message transmits client username and message
-    connection.send(bytes(message_to_send, "utf8"))
+    def __setup_flow(self):
+        self.launch_login(False)
+        self.connection = socket(AF_INET, SOCK_STREAM)  # init socket
+        self.connection.connect(TCP_ADDRESS)
+        login = "{}-{}".format(self.user, AUTH_TOKEN)
+        self.connection.send(bytes(login, "utf8"))  # send name when connected
+        user_confirm = self.connection.recv(BUFFERSIZE).decode("utf8")
 
+        while user_confirm != "ACCEPT":
+            print("Name in use: ( {} ) or version ( {} ) out-of-date".format(user_confirm, AUTH_TOKEN))
+            self.launch_login(True)
+            login = "{}-{}".format(self.user, AUTH_TOKEN)
+            self.connection.send(bytes(login, "utf8"))  # send name when connected
+            user_confirm = self.connection.recv(BUFFERSIZE).decode("utf8")
 
-def receive_message():
-    while True:
-        now = datetime.now()
-        time = "[" + now.strftime("%H:%M:%S") + "] "
-        try:
-            received_msg = connection.recv(BUFFERSIZE).decode("utf8")
-            printcolor(received_msg[0], time + received_msg)
-        except OSError:  # client left
-            break
+        receive_thread = Thread(target=self.receive_message, daemon=True)     # start thread for receiving messages
+        receive_thread.start()
 
+    def launch_login(self, retry):  # displays login prompt and returns user login
+        login_text = "Please choose a username to chat under."
+        if retry:
+            login_text = "Username is taken or client is out-of-date."
+        layout = [
+            [sg.Text(login_text)],
+            [sg.Text('Username: ', size=(15, 1)), sg.InputText('')],
+            [sg.Submit(), sg.Quit()]
+        ]
+        window = sg.Window('Login').Layout(layout)
+        event, values = window.Read()
+        if values[0] is None:
+            exit()
+        self.user = values[0]
+        window.close()
 
-def printcolor(sender, message):
-    back_color = "black"
-    try:
-        sender = sender.lower()
-        color = ord(sender) - 97
-        back_color = colors[color]
-    except:
+    def launch_chatbox(self):  # displays chatbox with custom user from login, calls send_message
+        while True:
+            event, values = self.window_main.Read()
+            if event in (None, 'Quit'):
+                self.send_message("/quit")  # sends out QUIT message from our client
+                break
+            elif event in self.buttons:
+                self.send_message(self.buttons[event])
+            else:
+                self.send_message(values[0])  # SUBMIT sends out message from our client
+        self.window_main.close()
+
+    def send_message(self, message_to_send):  # send_message transmits client username and message
+        self.connection.send(bytes(message_to_send, "utf8"))
+
+    def receive_message(self):
+        while True:
+            now = datetime.now()
+            time = "[" + now.strftime("%H:%M:%S") + "] "
+            try:
+                received_msg = self.connection.recv(BUFFERSIZE).decode("utf8")
+                try:
+                    self.printcolor(received_msg[0], time + received_msg)
+                except Exception as e:
+                    print(e)
+            except OSError:  # client left
+                break
+
+    def printcolor(self, sender, message):
         back_color = "black"
-    txt = window_main.FindElement("protocol").Widget
-    txt.tag_config(sender, background=back_color, foreground="white")
-    txt.insert("end", message + "\n", sender)
-    window_main.FindElement("protocol").Update("", append=True)
-
+        try:
+            sender = sender.lower()
+            color = ord(sender) - 97
+            back_color = self.colors[color]
+        except:
+            back_color = "black"
+        txt = self.window_main.FindElement("protocol").Widget
+        txt.tag_config(sender, background=back_color, foreground="white")
+        txt.insert("end", message + "\n", sender)
+        self.window_main.FindElement("protocol").Update("", append=True)
 
 # def sniffing():
 #     S = Sniffrr(15)
@@ -81,51 +121,13 @@ def printcolor(sender, message):
 #         S.CheckSniff()
 #         S.AddSniff()
 
-
-# socket info
-TCP_IP = "167.71.156.224"
-LOCAL_IP = "127.0.0.1"
-TCP_PORT = 33001
-TCP_ADDRESS = (TCP_IP, TCP_PORT)
-AUTH_TOKEN = "1111"
-BUFFERSIZE = 1024
-
-colors = ["black", "red", "green", "darkblue"]
-
-user = launch_login(False)   # get user from login window
-
-connection = socket(AF_INET, SOCK_STREAM)  # init socket
-connection.connect(TCP_ADDRESS)
-
-login = "{}-{}".format(user, AUTH_TOKEN)
-
-connection.send(bytes(login, "utf8"))    # send name when connected
-
-user_confirm = connection.recv(BUFFERSIZE).decode("utf8")
-
-while user_confirm != "ACCEPT":
-    print("Name in use: ( {} ) or version ( {} ) out-of-date".format(user_confirm, AUTH_TOKEN))
-    user = launch_login(True)
-    login = "{}-{}".format(user, AUTH_TOKEN)
-    connection.send(bytes(login, "utf8"))  # send name when connected
-    user_confirm = connection.recv(BUFFERSIZE).decode("utf8")
-
-receive_thread = Thread(target=receive_message, daemon=True)     # start thread for receiving messages
-receive_thread.start()
-
 # sniff_thread = Thread(target=sniffing)          # requires Scapy
 # sniff_thread.start()
 
 # TODO need to figure out how to stop user from being able to alter multiline box object on PySimpleGui docs
-layout_main = [
-    [sg.Multiline("", key="protocol", autoscroll=True, size=(80, 20))],
-    [sg.Text('%s: ' % user, size=(15, 1)), sg.InputText('', do_not_clear=False)],
-    [sg.Submit('Send'), sg.Quit(), sg.Button("dog", button_color=sg.TRANSPARENT_BUTTON,
-                                             image_filename="dog.png", image_size=(50, 50), image_subsample=2,
-                                             border_width=0)]
-]
-window_main = sg.Window('Chat').Layout(layout_main)
 
-launch_chatbox(user)    # start chatbox gui under client username
+
+if __name__ == "__main__":
+    c = Client()
 
 
--- server.py
+++ server.py
@@ -8,89 +8,90 @@ chatroom server
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
-
-def accept_incoming_connections():  # makes new thread for each client
-    while True:
-        client, client_address = server.accept()
-        print("{0}:{1} has connected".format(client, client_address))
-        addresses[client] = client_address
-        Thread(target=handle_client, args=(client,)).start()
-
-
-def handle_client(client):      # send chat setup info and receive name, broadcast messages
-    login = client.recv(BUFFERSIZE).decode("utf8")
-    name, token = login.split('-')
-    while name in names or AUTH_TOKEN not in token:
-        client.send(bytes("DENY", "utf8"))
-        login = client.recv(BUFFERSIZE).decode("utf8")
-        name, token = login.split('-')
-    client.send(bytes("ACCEPT", "utf8"))
-    names.append(name)
-
-    user_joined_message = ("{0} has joined the chat!".format(name))
-    broadcast_to_clients(user_joined_message)
-    clients[client] = name
-
-    while True:
-        try:
-            msg = client.recv(BUFFERSIZE).decode("utf8")
-        except OSError:  # client left
-            break
-        if "/quit" in msg:     # safely deletes client
-            client.close()
-            del clients[client]
-            names.remove(name)
-            broadcast_to_clients("{0} has left the chat.".format(name))
-            break
-        elif "/who" in msg:  # prints list of clients
-            print("/who called")
-            client.send(bytes(str(names), "utf8"))     # send message right back
-        elif "/help" in msg:
-            print("help called")
-            client.send(bytes(str(commands), "utf8"))  # send message right back
-        elif len(msg) > 100:
-            print("message size too big")
-            client.send(bytes("Message too large", "utf8"))  # send message right back
-        else:
-            broadcast_to_clients(msg, name)
-
-
-def broadcast_to_clients(message, user=""):     # send message to all clients
-    try:
-        clients_copy = clients
-        for sock in clients_copy:
-            try:
-                if user == "":
-                    sock.send(bytes(message, "utf8"))
-                else:
-                    sock.send(bytes("{0}: {1}".format(user, message), "utf8"))
-            except Exception as e:
-                del clients[sock]
-                pass
-    except Exception as e:
-        print(e)
-
-
-names = []
-clients = {}
-addresses = {}
-commands = ["/help", "/who", "/quit"]
 # socket info
 TCP_IP = "167.71.156.224"
 LOCAL_IP = "127.0.0.1"
 TCP_PORT = 33001
-TCP_ADDRESS = (TCP_IP, TCP_PORT)
+TCP_ADDRESS = (LOCAL_IP, TCP_PORT)
 AUTH_TOKEN = "1111"
 BUFFERSIZE = 1024
 
 
-server = socket(AF_INET, SOCK_STREAM)   # init socket
-server.bind(TCP_ADDRESS)
+class Server:
+
+    def __init__(self):
+        self.names = []
+        self.clients = {}
+        self.addresses = {}
+        self.commands = ["/help", "/who", "/quit"]
+        self.server = socket(AF_INET, SOCK_STREAM)   # init socket
+        self.server.bind(TCP_ADDRESS)
+        self.server.listen(5)
+        print("Waiting for connection...")
+        self.accept_thread = Thread(target=self.accept_incoming_connections)
+        self.accept_thread.start()
+        self.accept_thread.join()
+        self.server.close()
+
+    def accept_incoming_connections(self):  # makes new thread for each client
+        while True:
+            client, client_address = self.server.accept()
+            print("{0}:{1} has connected".format(client, client_address))
+            self.addresses[client] = client_address
+            Thread(target=self.handle_client, args=(client,)).start()
+
+    def handle_client(self, client):  # send chat setup info and receive name, broadcast messages
+        login = client.recv(BUFFERSIZE).decode("utf8")
+        name, token = login.split('-')
+        while name in self.names or AUTH_TOKEN not in token:
+            client.send(bytes("DENY", "utf8"))
+            login = client.recv(BUFFERSIZE).decode("utf8")
+            name, token = login.split('-')
+        client.send(bytes("ACCEPT", "utf8"))
+        self.names.append(name)
+
+        user_joined_message = ("{0} has joined the chat!".format(name))
+        self.broadcast_to_clients(user_joined_message)
+        self.clients[client] = name
+
+        while True:
+            try:
+                msg = client.recv(BUFFERSIZE).decode("utf8")
+            except OSError:  # client left
+                break
+            if "/quit" in msg:  # safely deletes client
+                client.close()
+                del self.clients[client]
+                self.names.remove(name)
+                self.broadcast_to_clients("{0} has left the chat.".format(name))
+                break
+            elif "/who" in msg:  # prints list of clients
+                print("/who called")
+                client.send(bytes(str(self.names), "utf8"))  # send message right back
+            elif "/help" in msg:
+                print("help called")
+                client.send(bytes(str(self.commands), "utf8"))  # send message right back
+            elif len(msg) > 100:
+                print("message size too big")
+                client.send(bytes("Message too large", "utf8"))  # send message right back
+            else:
+                self.broadcast_to_clients(msg, name)
+
+    def broadcast_to_clients(self, message, user=""):  # send message to all clients
+        try:
+            clients_copy = self.clients
+            for sock in clients_copy:
+                try:
+                    if user == "":
+                        sock.send(bytes(message, "utf8"))
+                    else:
+                        sock.send(bytes("{0}: {1}".format(user, message), "utf8"))
+                except Exception as e:
+                    del self.clients[sock]
+                    pass
+        except Exception as e:
+            print(e)
+
 
 if __name__ == "__main__":
-    server.listen(5)
-    print("Waiting for connection...")
-    accept_thread = Thread(target=accept_incoming_connections)
-    accept_thread.start()
-    accept_thread.join()
-    server.close()
+    S = Server()
