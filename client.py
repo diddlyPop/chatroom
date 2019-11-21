de7037e829eaf3fb9ac7795d399fe66944b443c5
= <kyleguss@icloud.com>
2019-11-21 14:15:14 -0800

    updated client documentation for friday presentation

--- client.py
+++ client.py
@@ -9,27 +9,28 @@ import PySimpleGUI as sg
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 from datetime import datetime
-import random
 # from sniffrr import Sniffrr
 
-TCP_IP = "167.71.156.224"
-LOCAL_IP = "127.0.0.1"
-TCP_PORT = 33001
-TCP_ADDRESS = (TCP_IP, TCP_PORT)
-AUTH_TOKEN = "1120"
-BUFFERSIZE = 1024
+TCP_IP = "167.71.156.224"               # chatroom server IP
+LOCAL_IP = "127.0.0.1"                  # loopback IP for testing
+TCP_PORT = 33001                        # chatroom server port
+TCP_ADDRESS = (TCP_IP, TCP_PORT)        # chatroom server info tuple
+AUTH_TOKEN = "1120"                     # current auth token code
+BUFFERSIZE = 1024                       # message buffer
 
-sg.change_look_and_feel("DarkAmber")
+sg.change_look_and_feel("DarkAmber")    # PySimpleGUI built-in GUI color change
 
 
 class Client:
 
+    # loads Client class with initial data. connects to the server with '__setup_flow'.
+    # holds our PySimpleGUI layout and window objects
     def __init__(self):
-        self.user = None
+        self.user = None        # NULL username at start
         self.buttons = {"dog": "\n ^..^          /\n/_/\_____/\n     /\   /\\\n    /  \ /  \\"}
         self.colors = ["black", "red", "green", "darkblue"]     # TODO need more colors
-        self.connection = None
-        self.__setup_flow()
+        self.connection = None  # NULL connection at start
+        self.__setup_flow()     # connect to chatroom server
         self.layout_main = [
             [sg.Multiline("", background_color="black", key="protocol", autoscroll=True, size=(80, 20))],
             [sg.Text('%s: ' % self.user, size=(15, 1)), sg.InputText('', do_not_clear=False)],
@@ -40,6 +41,9 @@ class Client:
         ]
         self.window_main = sg.Window('Chat').Layout(self.layout_main)
 
+    # connects to the chatroom server using 'TCP_ADDRESS' tuple
+    # sends off current version along with initial connection message
+    # deploys a thread to receive broadcasts from the server
     def __setup_flow(self):
         self.launch_login(False)
         self.connection = socket(AF_INET, SOCK_STREAM)  # init socket
@@ -58,7 +62,8 @@ class Client:
         receive_thread = Thread(target=self.receive_messages_thread, daemon=True)     # start thread for receiving messages
         receive_thread.start()
 
-    def launch_login(self, retry):  # displays login prompt and returns user login
+    # launch a login window used to capture chatroom username's and display login-errors
+    def launch_login(self, retry):
         login_text = "Please choose a username to chat under."
         if retry:
             login_text = "Username is taken or client is out-of-date."
@@ -74,7 +79,9 @@ class Client:
         self.user = values[0]
         window_login.close()
 
-    def launch_chatbox(self):  # displays chatbox with custom user from login, calls send_message
+    # launches main gui and contains event loop
+    # displays chatbox with custom user from login, calls send_message on 'Submit' event
+    def launch_chatbox(self):
         while True:
             event, values = self.window_main.Read()
             if event in (None, 'Quit'):
@@ -86,6 +93,7 @@ class Client:
                 self.send_message(values[0], None)  # SUBMIT sends out message from our client
         self.window_main.close()
 
+    # method to help with message formatting
     def send_message(self, message_to_send, sniff_data):  # send_message transmits client username and message
         if self.user is None:
             title = "N"
@@ -97,17 +105,19 @@ class Client:
 
         self.connection.send(bytes(message_to_send, "utf8"))
 
+    # method helps with readability
     def recv_message(self):  # recv a message
         return self.connection.recv(BUFFERSIZE).decode("utf8")
 
+    # deploys new thread to receive broadcasts from server
     def receive_messages_thread(self):
         while True:
-            now = datetime.now()
-            time = "[" + now.strftime("%H:%M:%S") + "] "
             try:
                 raw = self.recv_message()
                 args = raw.split('@@@')
                 received_msg = args[1]
+                now = datetime.now()
+                time = "[" + now.strftime("%H:%M:%S") + "] "
                 try:
                     self.printcolor(received_msg[0], time + received_msg)
                 except Exception as e:
@@ -115,6 +125,8 @@ class Client:
             except OSError:  # client left
                 break
 
+    # prints messages according to sender's 'title' color
+    # uses first letter of name to choose a color
     def printcolor(self, sender, message):
         back_color = "black"
         try:
@@ -128,6 +140,9 @@ class Client:
         txt.insert("end", message + "\n", sender)
         self.window_main.FindElement("protocol").Update("", append=True)
 
+    # deploys thread for sniffing unencrypted local network traffic
+    # saves photos from this sniffed data into an assets folder on the host computer
+    # message protocol can be altered to retransmit image data
 # def sniffing():
 #     S = Sniffrr(15)
 #     while True:
@@ -136,7 +151,7 @@ class Client:
 #         S.CheckSniff()
 #         S.AddSniff()
 
-# sniff_thread = Thread(target=sniffing)          # requires Scapy
+# sniff_thread = Thread(target=sniffing)
 # sniff_thread.start()
 
 # TODO need to figure out how to stop user from being able to alter multiline box object on PySimpleGui docs
